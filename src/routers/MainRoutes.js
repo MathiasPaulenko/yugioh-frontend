6d8029d87f2df63889896556861fa0db114fe450
mathias <mathias.paulenko@gmail.com>
2022-05-15 05:06:41 +0200

    new routes created, filters features

--- src/routers/MainRoutes.js
+++ src/routers/MainRoutes.js
@@ -1,8 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 
 import { Navbar } from '../components/common/Navbar';
+import { CardScreen } from '../components/screens/CardScreen';
 
 import { CollectionsScreen } from '../components/screens/CollectionsScreen'
+import { FiltersScreen } from '../components/screens/FiltersScreen';
 
 export const MainRoutes = () => {
 
@@ -13,9 +15,10 @@ export const MainRoutes = () => {
             <div className="container">
                 <Routes>
 
-                    <Route path="card/:serial_code" element={<CollectionsScreen />} />
                     <Route path="/" element={<CollectionsScreen />} />
                     <Route path="/collections" element={<CollectionsScreen />} />
+                    <Route path="/filters" element={<FiltersScreen />} />
+                    <Route path="card/:serial_code" element={<CardScreen />} />
 
                 </Routes>
             </div>
