c701b201fc351787a7fdf9c349eb37b7562e5ba3
mathias <mathias.paulenko@gmail.com>
2022-05-29 18:40:40 +0200

    all routes url about create and update card was added in main routers

--- src/routers/MainRoutes.js
+++ src/routers/MainRoutes.js
@@ -4,7 +4,20 @@ import { Navbar } from '../components/common/Navbar';
 import { CardScreen } from '../components/screens/CardScreen';
 
 import { CollectionsScreen } from '../components/screens/CollectionsScreen'
+import { AddCardScreen } from '../components/screens/AddCard/AddCardScreen';
 import { FiltersScreen } from '../components/screens/FiltersScreen';
+import { NormalAddCardScreen } from '../components/screens/AddCard/NormalAddCardScreen';
+import { FusionAddCardScreen } from '../components/screens/AddCard/FusionAddCardScreen';
+import { SynchroAddCardScreen } from '../components/screens/AddCard/SynchroAddCardScreen';
+import { XYZAddCardScreen } from '../components/screens/AddCard/XYZAddCardScreen';
+import { LinkAddCardScreen } from '../components/screens/AddCard/LinkAddCardScreen';
+import { TrapAddCardScreen } from '../components/screens/AddCard/TrapAddCardScreen';
+import { SpellAddCardScreen } from '../components/screens/AddCard/SpellAddCardScreen';
+import { TokenAddCardScreen } from '../components/screens/AddCard/TokenAddCardScreen';
+import { SkillAddCardScreen } from '../components/screens/AddCard/SkillAddCardScreen';
+import { RitualAddCardScreen } from '../components/screens/AddCard/RitualAddCardScreen';
+import { PendulumAddCardScreen } from '../components/screens/AddCard/PendulumAddCardScreen';
+import { UpdateCard } from '../components/screens/UpdateCard';
 
 export const MainRoutes = () => {
 
@@ -19,6 +32,20 @@ export const MainRoutes = () => {
                     <Route path="/collections" element={<CollectionsScreen />} />
                     <Route path="/filters" element={<FiltersScreen />} />
                     <Route path="card/:serial_code" element={<CardScreen />} />
+                    <Route path="card/update/:serial_code" element={<UpdateCard />} />
+                    
+                    <Route path="/add" element={<AddCardScreen />} />
+                    <Route path="/add/normal" element={<NormalAddCardScreen />} />
+                    <Route path="/add/fusion" element={<FusionAddCardScreen />} />
+                    <Route path="/add/synchro" element={<SynchroAddCardScreen />} />
+                    <Route path="/add/xyz" element={<XYZAddCardScreen />} />
+                    <Route path="/add/link" element={<LinkAddCardScreen />} />
+                    <Route path="/add/trap" element={<TrapAddCardScreen />} />
+                    <Route path="/add/spell" element={<SpellAddCardScreen />} />
+                    <Route path="/add/skill" element={<SkillAddCardScreen />} />
+                    <Route path="/add/token" element={<TokenAddCardScreen />} />
+                    <Route path="/add/pendulum" element={<PendulumAddCardScreen />} />
+                    <Route path="/add/ritual" element={<RitualAddCardScreen />} />
 
                 </Routes>
             </div>
