5b56cf11654c9b1eaa1b5568d7040f12c87d1de1
mathias <mathias.paulenko@gmail.com>
2022-06-29 22:12:35 +0200

    helpers added

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,4 +1,13 @@
 export const BASE_URL = 'http://127.0.0.1:8000/';
 
 export const LARGE_IMG_URL = 'https://storage.googleapis.com/ygoprodeck.com/pics/';
+export const SHORT_IMG_URL = 'https://storage.googleapis.com/ygoprodeck.com/pics_small/';
+export const YGO_IMG_SET = 'https://ygoprodeck.com/pics_sets/'
+export const YGO_PIC = 'https://ygoprodeck.com/pics/'
 export const IMG_EXT = '.jpg';
+
+export const YGO_API = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
+export const YGO_CARDSET = 'https://db.ygoprodeck.com/api/v7/cardsets.php'
+export const YGO_BASE = 'https://db.ygoprodeck.com/api/v7/'
+
+export const CONVERT_API = 'https://api.exchangerate-api.com/v4/latest/USD'
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,4 +1,4 @@
-import { BASE_URL } from './constants.js';
+// import { BASE_URL } from './constants.js';
 
 export const parsedDescription = (description) => {
     return description.replaceAll('\\\\', '\\').replaceAll('\\r', '').replaceAll('\\n', '<br>').replaceAll('\\', '');
@@ -6,8 +6,28 @@ export const parsedDescription = (description) => {
 
 
 export const getRarityIconOrNot = (rarity) => {
-    const rarities = ['Common', 'Rare', 'Super rare', 'Ultra rare'];
 
+    const rarities = [
+        'common',
+        'rare',
+        'super rare',
+        'ultra rare',
+        'extra secret rare',
+        'ghost rare',
+        'gold rare',
+        'parallel rarity',
+        'premium gold rare',
+        'prismatic secret rare',
+        'secret rare',
+        'ultra rare',
+        'ultimate rare',
+        'duel terminal',
+        'na',
+        'other',
+        'gold secret rare',
+
+    ];
+    rarity = rarity.toLowerCase();
     if (rarities.includes(rarity))
         return `/assets/img/rarity/${rarity}.png`;
     else
@@ -15,94 +35,38 @@ export const getRarityIconOrNot = (rarity) => {
 
 }
 
-export const fetchAddCard = (queries) => {
-    let flag = false;
-    if (Object.keys(queries).length !== 0) {
-        let queries_filters = {}
-        for (const query in queries) {
-            if (queries[query] !== '')
-                queries_filters[query] = queries[query];
+
+export function capitalize(text) {
+    return text.toLowerCase()
+        .split(' ')
+        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(' ');
+}
+
+
+
+export function getSetCodeFromCardSet(cardSet, set_name) {
+    let set_code = "";
+    cardSet.forEach(function (set) {
+        if (set.set_name === set_name) {
+            set_code = set.set_code;
         }
 
-        console.log(queries_filters);
+    })
 
-        if (!('type' in queries_filters)) {
-            flag = false;
-            return false;
+    return set_code;
+
+}
+
+export function getPriceFromCardSet(cardSet, set_name) {
+    let price = "";
+    cardSet.forEach(function (set) {
+        if (set.set_name === set_name) {
+            price = set.set_price;
         }
 
-        if (!('subtype' in queries_filters)) {
-            flag = false;
-            return false;
-        }
+    })
 
-        let reqBody = {}
-
-        if (queries_filters.type.toLowerCase() === 'skill') {
-            console.log("skill");
-        } else if (['trap', 'spell'].includes(queries_filters.type.toLowerCase())) {
-            console.log("trampa o magica");
-
-        } else {
-            if (queries_filters.subtype.toLowerCase().includes("pendulum")) {
-                console.log("pendulu");
-
-            } else if (queries_filters.subtype.toLowerCase().includes("link")) {
-                console.log("link");
-
-            } else {
-                console.log("monstruo o token");
-                reqBody = {
-                    card_number: queries_filters["card number"],
-                    serial_code: queries_filters["serial code"],
-                    name: queries_filters["name"],
-                    description: queries_filters["description"],
-                    attack: queries_filters["attack"],
-                    defence: queries_filters["defence"],
-                    type: queries_filters["type"],
-                    subtype: queries_filters["subtype"],
-                    race: queries_filters["race"],
-                    level: queries_filters["level"],
-                    attribute: queries_filters["attribute"],
-                    rarity: queries_filters["rarity"],
-                    archetype: queries_filters["archetype"],
-                    edition: queries_filters["edition"],
-                    set_name: queries_filters["set name"],
-                    img_code: queries_filters["img code"],
-                    amount: parseInt(queries_filters["amount"])
-                }
-
-                console.log(reqBody);
-
-            }
-        }
-
-
-        fetch(`${BASE_URL}collection/create`, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(reqBody)
-        }).then((response) => {
-            console.log(response);
-            if(response.ok){
-                console.log("ok");
-
-                flag = true;
-                return true;
-            }else{
-                console.log("pasas");
-                flag = false;
-                return false;
-            }
-        });
-        console.log('aqui tambien pasa');
-        console.log(flag);
-        return flag;
-    }else{
-        return false;
-    }
+    return price;
 
 }
